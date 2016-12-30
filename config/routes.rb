0e252bc5f7caad9bcd2a16773b97771709ab47c5
Rails Udemy Course <wilber.ventura@mavs.uta.edu>
2016-12-30 19:52:28 +0000

    Singup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   
   resources :articles
+  
+  get 'singup', to: "users#new"
+  
+  resources :users, except: [:new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
