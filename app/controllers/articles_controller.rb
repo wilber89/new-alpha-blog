926b2851eb255ab0de4a077828ff6a9fd9734fd7
Rails Udemy Course <wilber.ventura@mavs.uta.edu>
2016-12-30 16:20:07 +0000

    Add association btwn users and articles and debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,9 @@ class ArticlesController < ApplicationController
   
   def create
     #render plain: params[:article].inspect
+    debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfuly created"
       redirect_to article_path(@article)  
